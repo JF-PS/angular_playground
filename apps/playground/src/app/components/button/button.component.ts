24f6310b72e3c634ff547d2a48e78a1418f12e70
Cathelineaum <mat.cathelineau@gmail.com>
2022-11-03 16:29:48 +0100

    feature(button): bouton dynamique

--- apps/playground/src/app/components/button/button.component.ts
+++ apps/playground/src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'project-majeur-button',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./button.component.css'],
 })
 export class ButtonComponent {
-  
+  @Input() libelle: string | null = null;
 }
