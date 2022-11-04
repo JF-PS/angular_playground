ff34e2c98becd619fc9e45efc8af7fe8cf1d0d70
YAMEOGO Abdoul Moubarak <moubarak.webmaster@gmail.com>
2022-11-04 11:55:21 +0100

    feature(tags): optimisation de la gestion des tag

--- apps/playground/src/app/components/tags/tags.component.ts
+++ apps/playground/src/app/components/tags/tags.component.ts
@@ -1,11 +1,10 @@
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'project-majeur-tags',
   templateUrl: './tags.component.html',
   styleUrls: ['./tags.component.css']
 })
-export class TagsComponent {
-  @Input() tag: string | undefined;
+export class TagsComponent{
+  @Input() tags: string[] | undefined;
 }
