94020f7addfc587ef03eb4e847eedc44882caf58
YAMEOGO Abdoul Moubarak <moubarak.webmaster@gmail.com>
2022-11-04 16:24:55 +0100

    featured(dropdown): amélioration

--- apps/playground/src/app/components/dropdown/dropdown.component.ts
+++ apps/playground/src/app/components/dropdown/dropdown.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'project-majeur-dropdown',
   templateUrl: './dropdown.component.html',
   styleUrls: ['./dropdown.component.css'],
 })
-export class DropdownComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+export class DropdownComponent {
+  @Input() title: string | undefined;
+  @Input() options: string[] | undefined;
 }
