9dea2a965f5a74aa95a1dc3d0876cd41171fe36c
Cathelineaum <mat.cathelineau@gmail.com>
2022-11-17 16:29:07 +0100

    feature(card): mise en place des cards

---
+++ apps/playground/src/app/components/card-list/card-list.component.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
+import { Component, Input } from '@angular/core';
+import { CardService } from '../../services/card.service';
+
+@Component({
+  selector: 'project-majeur-card-list',
+  templateUrl: './card-list.component.html',
+  styleUrls: ['./card-list.component.css'],
+})
+export class CardListComponent {;
+}
