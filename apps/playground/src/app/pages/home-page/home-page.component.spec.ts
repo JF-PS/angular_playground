38d6b3a4c881af3c4f0aae1e3c51b1bca6643bc2
Cathelineaum <mat.cathelineau@gmail.com>
2022-11-25 14:48:59 +0100

    feature(jest) : ajout test récupération des données games

--- apps/playground/src/app/pages/home-page/home-page.component.spec.ts
+++ apps/playground/src/app/pages/home-page/home-page.component.spec.ts
@@ -1,22 +1,92 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { GameService } from '../../services/game.service';
+import GameType from '../../types/game-type';
 
 import { HomePageComponent } from './home-page.component';
 
 describe('HomePageComponent', () => {
   let component: HomePageComponent;
   let fixture: ComponentFixture<HomePageComponent>;
+  let gameListService: GameService;
 
   beforeEach(async () => {
+    gameListService = {} as GameService;
     await TestBed.configureTestingModule({
       declarations: [HomePageComponent],
+      providers: [{
+        provide: GameService, useValue: gameListService,
+      }]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePageComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('get Game list', () => {
+    const result = [{
+      developer: 'a',
+      freetogame_profile_url: 'a',
+      game_url: 'a',
+      genre: 'a',
+      id: 1,
+      platform: 'a',
+      publisher: 'a',
+      release_date: 'a',
+      short_description: 'a',
+      thumbnail: 'a',
+      title: 'a' 
+    },
+    {
+      developer: 'b',
+      freetogame_profile_url: 'b',
+      game_url: 'b',
+      genre: 'b',
+      id: 2,
+      platform: 'b',
+      publisher: 'b',
+      release_date: 'b',
+      short_description: 'b',
+      thumbnail: 'b',
+      title: 'b' 
+    },
+    ];
+
+    gameListService.getGameList = jest.fn().mockReturnValue( of(
+      [{
+        developer: 'a',
+        freetogame_profile_url: 'a',
+        game_url: 'a',
+        genre: 'a',
+        id: 1,
+        platform: 'a',
+        publisher: 'a',
+        release_date: 'a',
+        short_description: 'a',
+        thumbnail: 'a',
+        title: 'a' 
+      },
+      {
+        developer: 'b',
+        freetogame_profile_url: 'b',
+        game_url: 'b',
+        genre: 'b',
+        id: 2,
+        platform: 'b',
+        publisher: 'b',
+        release_date: 'b',
+        short_description: 'b',
+        thumbnail: 'b',
+        title: 'b' 
+      },]
+    ) );
+
+    fixture.detectChanges();
+    expect(component.popularGames).toEqual(result);
+  })
 });
