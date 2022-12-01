511fae0556298f5754cbad8ce01ec2503179da3e
YAMEOGO Abdoul Moubarak <moubarak.webmaster@gmail.com>
2022-12-01 11:27:18 +0100

    Update star rating final

--- apps/playground/src/app/components/button-with-modal/button-with-modal.component.ts
+++ apps/playground/src/app/components/button-with-modal/button-with-modal.component.ts
@@ -12,7 +12,7 @@ import { Component, Input } from '@angular/core';
 export class ButtonWithModalComponent {
 
   infoSpan: HTMLElement | null = document.getElementById('levelCount');
-  isAddFavorite = true;
+
   selectedRating = 0;
   stars = [
     {
