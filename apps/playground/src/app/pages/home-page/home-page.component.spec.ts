3f9f1b180009dd74009418468d7e7b9c63fc453a
Cathelineaum <mat.cathelineau@gmail.com>
2022-12-01 11:47:18 +0100

    feature(jest) : [WIP] TU login page

--- apps/playground/src/app/pages/home-page/home-page.component.spec.ts
+++ apps/playground/src/app/pages/home-page/home-page.component.spec.ts
@@ -1,8 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { of } from 'rxjs';
 import { GameService } from '../../services/game.service';
-import GameType from '../../types/game-type';
-
 import { HomePageComponent } from './home-page.component';
 
 describe('HomePageComponent', () => {
--- apps/playground/src/app/pages/login-page/login-page.component.spec.ts
+++ apps/playground/src/app/pages/login-page/login-page.component.spec.ts
@@ -1,22 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TranslateService } from '@ngx-translate/core';
+import { of } from 'rxjs';
+import { UserService } from '../../services';
 
 import { LoginPageComponent } from './login-page.component';
 
 describe('LoginPageComponent', () => {
   let component: LoginPageComponent;
   let fixture: ComponentFixture<LoginPageComponent>;
+  let userService: UserService;
+  let translate: TranslateService;
 
   beforeEach(async () => {
+    userService as UserService;
+    translate as TranslateService;
     await TestBed.configureTestingModule({
       declarations: [LoginPageComponent],
+      providers: [
+        {provide: UserService, useValue: userService},
+        {provide: TranslateService, useValue: translate}
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginPageComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('test register', () => {
+    userService.signup = jest.fn().mockReturnValue( of(true) );
+
+    fixture.detectChanges();
+    expect(component.isLoginForm).toEqual(true);
+    expect(component.myConnectChoice).toEqual('register');
+  })
 });
